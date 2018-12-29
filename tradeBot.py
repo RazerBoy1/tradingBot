5c8c79b2aa93e2681958bf6ea4eedcd4b552f85c
RazerBoy1 <RazerBoy1@gmail.com>
2018-12-29 04:04:29 +0100

    Update tradeBot.py

--- tradeBot.py
+++ tradeBot.py
@@ -1,6 +1,6 @@
 import time, math
 from decimal import Decimal, getcontext
-from kucoinAPIHelper import Client
+from kusell_coinPIHelper import Client
 
 class tradeBot(object):
     DECIMAL_PRECISION = 6
@@ -11,8 +11,10 @@ class tradeBot(object):
         self._percent_per_trade = percent_per_trade
 
         pair = self.get_trading_pair()
-        self.coinA = pair[0]
-        self.coinB = pair[1]
+        self.sell_coin = pair[0]
+        self.buy_coin = pair[1]
+
+        self.activeOrders = {}
 
         self.trade_loop()
 
@@ -20,11 +22,8 @@ class tradeBot(object):
         return self._symbol.split('-')
 
     def get_coin_balances(self):
-        stable_coin_balances = {'USDT': {'balance': 0.0},
-                                'TUSD': {'balance': 0.0},
-                                'USDC': {'balance': 0.0},
-                                'PAX': {'balance': 0.0},
-                                'DAI': {'balance': 0.0}}
+        stable_coin_balances = {self.sell_coin: {'balance': 0.0},
+                                self.buy_coin: {'balance': 0.0}}
 
         balances = self.h.get_all_balances()
 
@@ -35,31 +34,24 @@ class tradeBot(object):
 
         return stable_coin_balances
 
-    def get_decimal_representation(self, input_number):
-        dec = Decimal(input_number)
-        getcontext().prec = self.DECIMAL_PRECISION
+    def balance_above_one_dollar(self, balances):
+        for coin_name in balances:
+            if balances[coin_name]['balance'] > 1.0:
+                return True
 
-        return dec
-
-    def increment_one_at_last_digit(self, input_number):
-        dec = self.get_decimal_representation(input_number)
-        return dec.next_plus()
-
-    def decrement_one_at_last_digit(self, input_number):
-        dec = self.get_decimal_representation(input_number)
-        return dec.next_minus()
+        return False
 
     def get_optimal_buy_price(self, order_book, target_price):
         for order in order_book['BUY']:
             if order[0] < target_price:
-                return self.decrement_one_at_last_digit(order[0])
+                return self._decrement_one_at_last_digit(order[0])
 
         return target_price
 
     def get_optimal_sell_price(self, order_book, target_price):
         for order in order_book['SELL']:
             if order[0] > target_price:
-                return self.increment_one_at_last_digit(order[0])
+                return self._increment_one_at_last_digit(order[0])
 
         return target_price
 
@@ -67,27 +59,43 @@ class tradeBot(object):
         multiplier = 10 ** decimals
         return math.floor(n * multiplier) / multiplier
 
+    def _get_decimal_representation(self, input_number):
+        dec = Decimal(input_number)
+        getcontext().prec = 6
+
+        return dec
+
+    def _increment_one_at_last_digit(self, input_number):
+        dec = self._get_decimal_representation(input_number)
+        return dec.next_plus()
+
+    def _decrement_one_at_last_digit(self, input_number):
+        dec = self._get_decimal_representation(input_number)
+        return dec.next_minus()
+
     def trade_loop(self):
         while (True):
             balances = self.get_coin_balances()
-            order_book = self.h.get_order_book(self._symbol)
 
-            buy_price = self.get_optimal_buy_price(order_book, 1 - self._percent_per_trade)
-            sell_price = self.get_optimal_sell_price(order_book, 1 + self._percent_per_trade)
+            if self.balance_above_one_dollar(balances):
+                order_book = self.h.get_order_book(self._symbol)
 
-            balanceA = balances[self.coinA]['balance']
-            balanceB = balances[self.coinB]['balance']
+                buy_price = self.get_optimal_buy_price(order_book, 1 - self._percent_per_trade)
+                sell_price = self.get_optimal_sell_price(order_book, 1 + self._percent_per_trade)
 
-            amount_to_buy = self.round_down(balanceB / (1 - self._percent_per_trade), self.DECIMAL_PRECISION)
-            amount_to_sell = round(balanceA, 6)
+                buy_balance = balances[self.buy_coin]['balance']
+                sell_balance = balances[self.sell_coin]['balance']
 
-            if balanceA > 1:
-                self.h.create_buy_order(self._symbol, buy_price, amount_to_buy)
-            if balanceB > 1:
-                self.h.create_sell_order(self._symbol, sell_price, amount_to_sell)
+                amount_to_buy = self.round_down(buy_balance / (1 - self._percent_per_trade), self.DECIMAL_PRECISION)
+                amount_to_sell = round(sell_balance, 6)
 
-            time.sleep(0.25)
+                if sell_balance > 1.0:
+                    self.h.create_buy_order(self._symbol, buy_price, amount_to_buy)
+                if buy_balance > 1.0:
+                    self.h.create_sell_order(self._symbol, sell_price, amount_to_sell)
 
-t = tradeBot('USDT-PAX', 0.1)
+            time.sleep(0.5)
+
+t = tradeBot('USDT-PAX', 0.005)
 
 # Ne undercutat yourself bitch
